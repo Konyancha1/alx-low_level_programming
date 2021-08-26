2c3de6cb1d7274d3040d362029aa6275f083e8d4
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-08-26 18:17:23 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
+#include<stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-#include <stdio.h>
-int main (void)
+int main(void)
 {
-  char *s = "\"Programming is like building a multilingual puzzle";
-  puts(s);
-  return (0);
+        char *s = "\"Programming is like building a multilingual puzzle";
+        puts(s);
+        return (0);
 }
-
-  
-  
