d66c24ca5d0ca8b5e61ea9e1881eeca117cfe197
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-08-26 19:08:28 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ long int longintType;
 long long int longlongintType;
 float floatType;
 
-  // sizeof evaluates the size of a variable
 printf("Size of char: %zu byte(s)\n", sizeof(charType));
 printf("Size of int: %zu byte(s)\n", sizeof(intType));
 printf("Size of long int: %zu byte(s)\n", sizeof(longintType));
