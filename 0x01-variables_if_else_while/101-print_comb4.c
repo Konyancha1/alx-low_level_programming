60340beb7632458412e5460187034e6ebfc575bb
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-08-29 01:18:16 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,38 +8,38 @@
  */
 int main(void)
 {
-  int i, e, g;
+int a, b, c;
 
-  i = 48;
-  e = 48;
-  g = 48;
+a = 48;
+b = 48;
+c = 48;
 
-  while (e < 58)
-    {
-      i = 48;
-      while (i < 58)
-	{
-	  g = 48;
-	  while (g < 58)
-	    {
-	      if (e != i && e != g && i != g && e < i && i < g)
-		{
-		  putchar(e);
-		  putchar(i);
-		  putchar(g);
-		  if (i == 56 && e == 55 && g == 57)
-		    {
-		      break;
-		    }
-		  putchar(',');
-		  putchar(' ');
-		}
-	      g++;
-	    }
-	  i++;
-	}
-      e++;
-    }
-  putchar('\n');
-  return (0);
+while (b < 58)
+{
+a = 48;
+while (a < 58)
+{
+c = 48;
+while (c < 58)
+{
+if (b != a && b != c && a != c && b < a && a < c)
+{
+putchar(b);
+putchar(a);
+putchar(c);
+if (a == 56 && b == 55 && c == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+c++;
+}
+a++;
+}
+b++;
+}
+putchar('\n');
+return (0);
 }
