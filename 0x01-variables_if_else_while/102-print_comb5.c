604642f4a23e7d73c12302b8e85f190940ae7f09
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-08-29 01:28:24 +0200

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 00 to 99.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a, b, c, d, Y, Z;
+a = b = c = d = 48;
+while (d < 58)
+{
+c = 48;
+while (c < 58)
+{
+b = 48;
+while (b < 58)
+{
+a = 48;
+while ( < 58)
+{
+Y = (d * 10) + c;
+Z = (b * 10) + a;
+if (Y < Z)
+{
+putchar(d);
+putchar(c);
+putchar(' ');
+putchar(b);
+putchar(a);
+if (d == 57 && c == 56 && b == 57 && a == 57)
+break;
+putchar(',');
+putchar(' ');
+}
+a++;
+}
+b++;
+}
+c++;
+}
+d++;
+}
+putchar('\n');
+return (0);
+}
