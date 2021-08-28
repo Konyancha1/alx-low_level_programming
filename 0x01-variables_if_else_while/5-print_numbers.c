93e233c8c20f9809da09691a1d4056b08e12e79c
Konyancha1 <kmuua791@gmail.com>
2021-08-28 23:26:42 +0200

    Print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++);
+{
+  printf("%d\n", a)
+}
+ return (0);
+}
+
+
+
+  
