f0b2ac7c19da3ba9934cf9c11f2f5fc9f8c04a3a
Konyancha1 <kmuua791@gmail.com>
2021-08-28 23:12:20 +0200

    Printing alphabet upper and lower cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Prints the alphabet.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char a[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
+
+for (i = 0; i < 52; i++)
+{
+putchar(a[i]);
+}
+putchar('\n');
+}
