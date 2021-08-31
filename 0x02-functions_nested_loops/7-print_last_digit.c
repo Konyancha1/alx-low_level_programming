72618ac68d89abafb19a102f90cc3f0de895c822
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-08-31 18:52:42 +0200

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,33 +1,25 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - check the code for ALX School students.
+ * print_last_digit - Prints the last digit
+ * of a number.
  *
- * Return: Always 0.
+ * @n: input number as an integer.
+ *
+ * Return: last digit.
  */
-int main(void)
-{
-int r;
 
-print_last_digit(98);
-print_last_digit(0);
-r = print_last_digit(-1024);
-_putchar('0' + r);
-_putchar('\n');
-return (0);
-}
 int print_last_digit(int a)
 {
-  int b;
-  b = a % 10;
-  if (b < 0)
-    {
-      _putchar(-b + 48);
-      return (-b);
-    }
-  else
-    {
-      _putchar(b + 48);
-      return (b);
-    }
+int b;
+b = a % 10;
+if (b < 0)
+{
+_putchar(-b + 48);
+return (-b);
+}
+else
+{
+_putchar(b + 48);
+return (b);
+}
 }
