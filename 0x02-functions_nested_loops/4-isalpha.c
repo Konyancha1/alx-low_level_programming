6bda0f417c574266bbed1584ddbde4703f6f18cf
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-08-31 17:00:41 +0200

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,25 +2,9 @@
 /**
  * isalpha - shows 1 if lowercase or uppercase letter is present
  * shows 0 if otherwise
- * main - check the code for ALX School students.
  *
  * Return: Always 0.
  */
-int main(void)
-{
-  int r;
-
-  r = _isalpha('H');
-  _putchar(r + '0');
-  r = _isalpha('o');
-  _putchar(r + '0');
-  r = _isalpha(108);
-  _putchar(r + '0');
-  r = _isalpha(';');
-  _putchar(r + '0');
-  _putchar('\n');
-  return (0);
-}
 int _isalpha(int c)
 {
   if (c >= 65 && c <= 122)
