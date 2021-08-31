d9aed953baeebaa4a221c72b41ae8fa95b609dd8
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-08-31 16:40:22 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,14 @@
 #include "main.h"
 /**
+ * main - check the code for ALX School students.
  * print_alphabet - Prints the alphabet in lowercase.
- *
  * Return: Always 0 (Success)
  */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
 void print_alphabet(void)
 {
 int i;
