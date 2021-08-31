2fce7cd3110c9fba1d8c790b2bc9cfc5a11b3ffe
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-08-31 15:38:19 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * main - check the code for ALX School students.
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  *
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
-  int a, b;
+int a, b;
 
-  for (a = 0; a <= 9; a++)
-    {
-      for (b = 97; b <= 122; b++)
-	{
-	  _putchar(b);
-	}
-      _putchar('\n');
-    }
+for (a = 0; a <= 9; a++)
+{
+for (b = 97; b <= 122; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+}
 }
