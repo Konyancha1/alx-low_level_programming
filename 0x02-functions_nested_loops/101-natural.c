f1618d6d5a5f2da4f2e9d5f0ddbdd3efa9c180eb
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-08-31 21:26:58 +0200

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,13 +7,12 @@
  */
 int main(void)
 {
-  int x, y;
-  for (x =1; x < 1024; x++)
-    {
-      if ((x % 3) == 0 || (x % 5) == 0)
-	y += x;
-    }
-  printf("%d\n", y);
-  return (0);
+int x, y;
+for (x =1; x < 1024; x++)
+{
+if ((x % 3) == 0 || (x % 5) == 0)
+y += x;
 }
-
+printf("%d\n", y);
+return (0);
+}
