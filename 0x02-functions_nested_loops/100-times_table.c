5ca133c0d722ab28cf81dfaf18ac66fdba35d31d
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-08-31 20:52:56 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,62 +1,45 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - check the code for ALX School students.
+ * print_times_table - Prints the n times table
  *
- * Return: Always 0.
+ * @n: number times table (0 < n <= 15)
+ *
+ * Return: no return
  */
-int main(void)
-{
-  print_times_table(3);
-  _putchar('\n');
-  print_times_table(5);
-  _putchar('\n');
-  print_times_table(98);
-  _putchar('\n');
-  print_times_table(12);
-  return (0);
-}
 void print_times_table(int n)
 {
-  int a, b, c;
+int a, b, c;
   
-  if (n >= 0 && n <= 15)
-    {
-      for (a = 0; a <= n; a++)
-	{
-	  _putchar(48);
-	  for (b = 1;b <= n;b++)
-	    {
-	      c = a * b;
-	      _putchar(44);
-	      _putchar(32);
-	      if (c <= 9)
-		{
-		  _putchar(32);
-		  _putchar(32);
-		  _putchar(c + 48);
-		}
-	      else if (c <= 99)
-		{
-		  _putchar(32);
-		  _putchar((c / 10) + 48);
-		  _putchar((c % 10) + 48);
-		}
-	      else
-		{
-		  _putchar(((c / 100) % 100) + 48);
-		  _putchar(((c / 10) % 10) + 48);
-		  _putchar((c % 10) + 48);
-		}
-	      _putchar('\n');
-	    }
-	}
-    }
+if (n >= 0 && n <= 15)
+{
+for (a = 0; a <= n; a++)
+{
+_putchar(48);
+for (b = 1; b <= n; b++)
+{
+c = a * b;
+_putchar(44);
+_putchar(32);
+if (c <= 9)
+{
+_putchar(32);
+_putchar(32);
+_putchar(c + 48);
 }
-
-			   
-	      
-	       
-	  
-	  
-	  
+else if (c <= 99)
+{
+_putchar(32);
+_putchar((c / 10) + 48);
+_putchar((c % 10) + 48);
+}
+else
+{
+_putchar(((c / 100) % 100) + 48);
+_putchar(((c / 10) % 10) + 48);
+_putchar((c % 10) + 48);
+}
+_putchar('\n');
+}
+}
+}
+}
