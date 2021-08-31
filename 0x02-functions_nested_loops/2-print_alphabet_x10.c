3002bf7fb7886190c198d7e70eae86e72f09f50d
Konyancha1 <kmuua791@gmail.com>
2021-08-31 15:19:22 +0200

    Print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  print_alphabet_x10();
+  return (0);
+}
+
+void print_alphabet_x10(void);
+{ 
+  int a, b)
+
+ for (a = 0; a < 11; a++)
+ {
+   for (b = 97; b <= 122; b++)
+     {
+       _putchar(b);
+     }
+   _putchar('\n')
+     }
+}
+
+			  
+
+  
+  
