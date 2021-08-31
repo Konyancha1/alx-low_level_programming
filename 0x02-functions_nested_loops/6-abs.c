afca3ace54113eaadbdb052957f0f1145c1c8cc8
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-08-31 18:34:13 +0200

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
  * _abs - Computes the absolute value
  * of an integer.
  *
@@ -11,31 +7,14 @@
  *
  * Return: ab
  */
-int main(void)
-{
-int r;
-
-r = _abs(-1);
-printf("%d\n", r);
-r = _abs(0);
-printf("%d\n", r);
-r = _abs(1);
-printf("%d\n", r);
-r = _abs(-98);
-printf("%d\n", r);
-return (0);
-}
 int _abs(int i)
 {
-  if (i >= 0)
-    {
-      return (i);
-    }
-  else
-    {
-      return (i *= -1);
-    }
+if (i >= 0)
+{
+return (i);
 }
-
-	
-  
+else
+{
+return (i *= -1);
+}
+}
