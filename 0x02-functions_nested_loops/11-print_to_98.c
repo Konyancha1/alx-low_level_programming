5ab1cd54dba50c2d5666edf3a01b65358f1b2fb9
Konyancha1 <kmuua791@gmail.com>
2021-08-31 20:12:09 +0200

    Prints numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  print_to_98(0);
+  print_to_98(98);
+  print_to_98(111);
+  print_to_98(81);
+  print_to_98(-10);
+  return (0);
+}
+void print_to_98(int n)
+{
+  if (n > 98)
+    {
+      for (; n > 98; n--)
+	{
+	  printf("%d, ", n);
+	}
+    }
+  else if (n < 98)
+    {
+      for (; n < 98; n++)
+	{
+	  printf("%d, ", n);
+	}
+    }
+  printf("%d\n", n);
+}
