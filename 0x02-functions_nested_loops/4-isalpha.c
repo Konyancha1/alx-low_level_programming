3e2cd96d3024739aef9fcd6008f62455b97a6e95
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-08-31 17:03:28 +0200

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
- * isalpha - shows 1 if lowercase or uppercase letter is present
- * shows 0 if otherwise
+ * _isalpha - Shows 1 if the input is a
+ * letter Another cases, shows 0
  *
+ * @c: The character in ASCII code
  * Return: Always 0.
  */
 int _isalpha(int c)
 {
-  if (c >= 65 && c <= 122)
-    {
-      return (1);
-    }
-  else
-    {
-      return (0);
-    }
-  _putchar('\n');
+if (c >= 65 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
 }
