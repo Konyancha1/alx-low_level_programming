a718446e2b06a29005c1bee029edb66276d96674
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-08-31 15:36:49 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,6 @@
  */
 void print_alphabet_x10(void)
 {
-<<<<<<< HEAD
   int a, b;
 
   for (a = 0; a <= 9; a++)
@@ -18,25 +17,3 @@ void print_alphabet_x10(void)
       _putchar('\n');
     }
 }
-=======
-  print_alphabet_x10();
-  return (0);
-}
-
-void print_alphabet_x10(void);
-({
-  int a, b;
-
- for (a = 0; a < 11; a++)
- {
-   for (b = 97; b <= 122; b++)
-     {
-       _putchar(b);
-     }
-   _putchar('\n');
-     }
-)}
->>>>>>> b865dcc682d5fe963ce0f22b50a4b0669119ab0e
-
-  
-  
