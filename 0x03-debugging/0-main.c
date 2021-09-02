4c2b7ac2da9a7369643581a5b012ffc9368c452c
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-09-02 17:20:50 +0200

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,19 +7,10 @@
 
 int main(void)
 {
-<<<<<<< HEAD
-  int i;
-
-  i = 0;
-  positive_or_negative(i);
-
-  return (0);
-=======
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
 
 	return (0);
->>>>>>> 5dec8247f1267dd27c89c453d3e9baca408a2b7f
 }
