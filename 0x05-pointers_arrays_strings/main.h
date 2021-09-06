d4418a007cd9b179fd94fdf4b725d7131b066c81
Konyancha1 <kmuua791@gmail.com>
2021-09-06 20:44:34 +0200

    Convert strings to integers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+
 #ifndef main_h
 #define main_h
 
@@ -12,5 +13,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif
