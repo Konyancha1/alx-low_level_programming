2466928355e75e8066546e833c5b348f29efad70
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-09-09 19:49:35 +0200

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,29 @@
 #include "main.h"
 /**
  * leet - encodes a string into 1337
- * @s: input string
- * Return: the pointer to dest
+ * @s: input string.
+ * Return: the pointer to dest.
  */
+
 char *leet(char *s)
 {
-  int size, a;
-  int small_letters[] = {97, 101, 111, 116, 108};
-  int upp_letters[] = {65, 69, 79, 84, 76};
-  int numbers[] = {52,51,48,55,49};
+	int size = 0, a;
+	int small_letters[] = {97, 101, 111, 116, 108};
+	int upp_letters[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
 
-  while (*(s + size) != '\0')
-    {
-      for (a = 0; a < 5; a++)
+	while (*(s + size) != '\0')
 	{
-	  if (*(s + size) == small_letters[a] || *(s + size) == upp_letters[a])
-	    {
-	      *(s + size) = numbers[a];
-	      break;
-	    }
+		for (a = 0; a < 5; a++)
+		{
+			if (*(s + size) == small_letters[a] || *(s + size) == upp_letters[a])
+			{
+				*(s + size) = numbers[a];
+				break;
+			}
+		}
+		size++;
 	}
-      size++;
-    }
-  return (s);
+
+	return (s);
 }
