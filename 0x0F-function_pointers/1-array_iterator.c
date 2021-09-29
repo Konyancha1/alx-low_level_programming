2121e541421d2ae85afaca424450a21043b5d0df
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-09-30 00:51:55 +0200

    Executes a function given as a parameter

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 unsigned int a;
 
 if (array && action)
-for (a = 0; a < size, a++)
+for (a = 0; a < size; a++)
 action(array[a]);
 }
