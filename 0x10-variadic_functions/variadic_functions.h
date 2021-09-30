1da43a853be81b53e9fb6bfde682581e46b051df
Konyancha1 <kmuua791@gmail.com>
2021-09-30 19:09:22 +0200

    Sum of all its parameters

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef _variadic_functions_h
+#define _variadic_functions_h
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif
