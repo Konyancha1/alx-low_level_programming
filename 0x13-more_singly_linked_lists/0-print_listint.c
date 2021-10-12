554cf3a72c47bd8f000216083e1fc87c30ead0b3
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-10-12 15:29:40 +0200

    Prints content of a list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * print_listint - prints all the elements of a list.
  * @h: head of a list.
@@ -8,13 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+size_t nodes = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		nodes++;
-	}
-	return (nodes);
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+nodes++;
+}
+return (nodes);
 }
