528119f0fb928b4a0a936be5122a89b41930f8fc
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-10-12 15:40:27 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,13 +6,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t nodes = 0;
+size_t no_node = 0;
 
 while (h != NULL)
 {
 printf("%d\n", h->n);
 h = h->next;
-nodes++;
-}
-return (nodes);
+no_node++;
+};
+return (no_node);
 }
